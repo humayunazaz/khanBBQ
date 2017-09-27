4295367d79a326a232eb8c2f0b242ef8497336af
Humayun Azaz <humayun.azaz@gmail.com>
2017-09-27 19:44:54 +0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,4 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyBx1Wx15meawSJyIdeZzFJ9Q9u0Ph8x5_c",
-    authDomain: "khanbbq-921fa.firebaseapp.com",
-    databaseURL: "https://khanbbq-921fa.firebaseio.com",
-    projectId: "khanbbq-921fa",
-    storageBucket: "khanbbq-921fa.appspot.com",
-    messagingSenderId: "23141409632"
-  }
 };
